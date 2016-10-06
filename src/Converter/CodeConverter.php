9193534849367278056de5c92e131aa58e8defe7
Andres Rey <andreskrey@gmail.com>
2016-10-06 22:56:15 +0100

    More progress on CodeConverter

--- src/Converter/CodeConverter.php
+++ src/Converter/CodeConverter.php
@@ -33,49 +33,9 @@ class CodeConverter implements ConverterInterface
         // Store the content of the code block in an array, one entry for each line
 
         $markdown = '';
-        $element->getNext($element->node);
-        $code_content = html_entity_decode($element->getChildrenAsString());
-        $code_content = str_replace(array('<code>', '</code>'), '', $code_content);
-        $code_content = str_replace(array('<pre>', '</pre>'), '', $code_content);
+        $code_content = html_entity_decode($element->getValue());
 
-        $lines = preg_split('/\r\n|\r|\n/', $code_content);
-        $total = count($lines);
-
-        // If there's more than one line of code, prepend each line with four spaces and no backticks.
-        if ($total > 1 || $element->getTagName() === 'pre') {
-            // Remove the first and last line if they're empty
-            $first_line = trim($lines[0]);
-            $last_line = trim($lines[$total - 1]);
-            $first_line = trim($first_line, '&#xD;'); //trim XML style carriage returns too
-            $last_line = trim($last_line, '&#xD;');
-
-            if (empty($first_line)) {
-                array_shift($lines);
-            }
-
-            if (empty($last_line)) {
-                array_pop($lines);
-            }
-
-            $count = 1;
-            foreach ($lines as $line) {
-                $line = str_replace('&#xD;', '', $line);
-                $markdown .= '    ' . $line;
-                // Add newlines, except final line of the code
-                if ($count !== $total) {
-                    $markdown .= "\n";
-                }
-                $count++;
-            }
-            $markdown .= "\n";
-        } else {
-            // There's only one line of code. It's a code span, not a block. Just wrap it with backticks.
-            $markdown .= '`' . $lines[0] . '`';
-        }
-
-        if ($element->getTagName() === 'code') {
-            $markdown = "\n" . $markdown . "\n";
-        }
+        $markdown .= '```' . $language . "\n" . $code_content . "\n" . '```';
 
         return $markdown;
     }
