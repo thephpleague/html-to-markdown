02e0bb9c7cc01f0e402b04407628d24754eceaf2
Andres Rey <andreskrey@gmail.com>
2016-10-11 18:04:44 +0100

    Cleaning up code

--- src/Converter/PreformattedConverter.php
+++ src/Converter/PreformattedConverter.php
@@ -15,32 +15,32 @@ class PreformattedConverter implements ConverterInterface
     {
         $markdown = '';
 
-        $code_content = html_entity_decode($element->getChildrenAsString());
-        $code_content = str_replace(array('<pre>', '</pre>'), '', $code_content);
+        $pre_content = html_entity_decode($element->getChildrenAsString());
+        $pre_content = str_replace(array('<pre>', '</pre>'), '', $pre_content);
 
         /*
          * Checking for the code tag.
-         * Usually pre tags are used along with code tags. This conditional will check for converted code tags,
+         * Usually pre tags are used along with code tags. This conditional will check for already converted code tags,
          * which use backticks, and if those backticks are at the beginning and at the end of the string it means
          * there's no more information to convert.
          */
 
-        $firstBacktick = strpos(trim($code_content), '`');
-        $lastBacktick = strrpos(trim($code_content), '`');
-        if ($firstBacktick === 0 && $lastBacktick === strlen(trim($code_content)) - 1) {
-            return $code_content;
+        $firstBacktick = strpos(trim($pre_content), '`');
+        $lastBacktick = strrpos(trim($pre_content), '`');
+        if ($firstBacktick === 0 && $lastBacktick === strlen(trim($pre_content)) - 1) {
+            return $pre_content;
         }
 
         // If the execution reaches this point it means it's just a pre tag, with no code tag nested
 
         // Checking if the string has multiple lines
-        $lines = preg_split('/\r\n|\r|\n/', $code_content);
+        $lines = preg_split('/\r\n|\r|\n/', $pre_content);
         if (count($lines) > 1) {
             // Multiple lines detected, adding three backticks and newlines
-            $markdown .= '```' . "\n" . $code_content . "\n" . '```';
+            $markdown .= '```' . "\n" . $pre_content . "\n" . '```';
         } else {
             // One line of code, wrapping it on one backtick.
-            $markdown .= '`' . $code_content . '`';
+            $markdown .= '`' . $pre_content . '`';
         }
 
         return $markdown;
