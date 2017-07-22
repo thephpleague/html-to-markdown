834b1c5206d7f978b8653a5caed458707c29813e
Sergei Miami <miami@blackcrystal.net>
2017-07-22 16:43:46 +0300

    if break is inside formatting tag, change it to space

--- src/Converter/HardBreakConverter.php
+++ src/Converter/HardBreakConverter.php
@@ -28,6 +28,7 @@ class HardBreakConverter implements ConverterInterface, ConfigurationAwareInterf
      */
     public function convert(ElementInterface $element)
     {
+        if (in_array($element->getParent()->getTagName(),array('i','u','b','em','strong','h1', 'h2', 'h3', 'h4', 'h5', 'h6'))) return ' ';
         return $this->config->getOption('hard_break') ? "\n" : "  \n";
     }
 
